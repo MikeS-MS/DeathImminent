972dccbf3b2b33d441286b13642dc2ac416b4b2f
MikeS <mikemediastudios@gmail.com>
2021-04-01 08:55:57 +0300

    Synced door widget, added a demo level for the university presentation, moved assets around to make it more organized

--- Source/HorrorEscape/SlidingDoor.h
+++ Source/HorrorEscape/SlidingDoor.h
@@ -3,43 +3,13 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Kismet/KismetSystemLibrary.h"
+#include "ComponentManipulation.h"
 #include "Door.h"
 #include "SlidingDoor.generated.h"
 
 /**
  * 
  */
-
-USTRUCT(BlueprintType)
-struct HORRORESCAPE_API FMovePosition
-{
-	GENERATED_BODY()
-
-	FMovePosition() {}
-	
-	/*Enable moving on this axis. (Enable editing of other values in editor only.)*/
-	UPROPERTY(BlueprintReadWrite, EditAnywhere)
-	bool bEnableMoving{ false };
-
-	/*-1 moves the door in a negative direction on the axis, 0 means no movement and (+)1 moves it in a positive direction on the axis*/
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (EditCondition = "bEnableMoving", ClampMin = "-1", ClampMax = "1"))
-	int MoveDirection { 0 };
-
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (EditCondition = "bEnableMoving"))
-	bool bEnableAdditive{ false };
-	
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (EditCondition = "bEnableAdditive", ClampMin = "0"))
-	float AdditiveMoveValue{ 0.f };
-	
-	/*Enable custom value to move the door. OVERRIDE*/
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (EditCondition = "bEnableMoving"))
-	bool bCustomMoveValue { false };
-
-	/*Custom value to move the door by. Default = Position +- SizeOfMesh*/
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (EditCondition = "bCustomMoveValue", ClampMin = "0"))
-	float MoveBy{ 0.f };
-};
 
 UCLASS( ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class HORRORESCAPE_API USlidingDoor : public UDoor
