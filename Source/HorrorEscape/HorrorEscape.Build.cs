2a06ccb9f62e229ff66442a743414987c55ce877
MikeS-MS <mmstudiosg441@gmail.com>
2022-09-30 09:25:49 +0300

    Pre-upgrade to 5.0. Added a way to send payload to menus so they can have events called on them when the player closes them.

--- Source/HorrorEscape/HorrorEscape.Build.cs
+++ Source/HorrorEscape/HorrorEscape.Build.cs
@@ -8,15 +8,16 @@ public class HorrorEscape : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "OnlineSubsystem", "OnlineSubsystemSteam", "Steamworks" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { "FileHelper" });
 
+		//DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
 		// Uncomment if you Keys using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
 		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+		//PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
 		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
 	}
