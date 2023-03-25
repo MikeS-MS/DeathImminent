7e7d6b722cdd3eeff0740de5f6fc07a1b179e249
MikeS-MS <mmstudiosg441@gmail.com>
2023-03-25 17:45:58 +0200

    Changed:  - Base for all items is now an actor  - Item interactions in the inventory and the display in the ui have been made to work with the new base item

--- Source/HorrorEscape/Private/Misc/ReadWriteProcess.cpp
+++ Source/HorrorEscape/Private/Misc/ReadWriteProcess.cpp
@@ -1,11 +1,31 @@
 #include "Misc/ReadWriteProcess.h"
+#include "Systems/ServerManagingSubsystem.h"
 
-FReadWriteProcess::FReadWriteProcess(const FString& InURL, const FString& InParams, bool InHidden, bool InCreatePipes) : FMonitoredProcess(InURL, InParams, InHidden, InCreatePipes) {}
+FReadWriteProcess::FReadWriteProcess(UServerManagingSubsystem* ServerManagingSubsystem,
+									 const FString& InURL,
+									 const FString& InParams,
+									 bool InHidden,
+									 bool InCreatePipes) : FMonitoredProcess(InURL, InParams, InHidden, InCreatePipes)
+{
+	this->ServerManagingSubsystem = ServerManagingSubsystem;
+}
 
-FReadWriteProcess::FReadWriteProcess(const FString& InURL, const FString& InParams, const FString& InWorkingDir,
-	bool InHidden, bool InCreatePipes) : FMonitoredProcess(InURL, InParams, InWorkingDir, InHidden, InCreatePipes) {}
+FReadWriteProcess::FReadWriteProcess(UServerManagingSubsystem* ServerManagingSubsystem, 
+									 const FString& InURL, 
+									 const FString& InParams, 
+									 const FString& InWorkingDir,
+									 bool InHidden, 
+									 bool InCreatePipes) : FMonitoredProcess(InURL, InParams, InWorkingDir, InHidden, InCreatePipes)
+{
+	this->ServerManagingSubsystem = ServerManagingSubsystem;
+}
 
 bool FReadWriteProcess::SendMessage(const FString& Message)
 {
 	return FPlatformProcess::WritePipe(WritePipe, Message);
+}
+
+void FReadWriteProcess::OnCompleted(int32 returnCode)
+{
+
 }
--- Source/HorrorEscape/Public/Misc/ReadWriteProcess.h
+++ Source/HorrorEscape/Public/Misc/ReadWriteProcess.h
@@ -4,11 +4,17 @@
 #include "Misc/MonitoredProcess.h"
 //#include "ReadWriteProcess.generated.h"
 
+class UServerManagingSubsystem;
+
 class HORRORESCAPE_API FReadWriteProcess : public FMonitoredProcess
 {
+private:
+	UServerManagingSubsystem* ServerManagingSubsystem;
 public:
-	FReadWriteProcess(const FString& InURL, const FString& InParams, bool InHidden, bool InCreatePipes = true);
-	FReadWriteProcess(const FString& InURL, const FString& InParams, const FString& InWorkingDir, bool InHidden, bool InCreatePipes = true);
+	FReadWriteProcess(UServerManagingSubsystem* ServerManagingSubsystem, const FString& InURL, const FString& InParams, bool InHidden, bool InCreatePipes = true);
+	FReadWriteProcess(UServerManagingSubsystem* ServerManagingSubsystem, const FString& InURL, const FString& InParams, const FString& InWorkingDir, bool InHidden, bool InCreatePipes = true);
 
 	bool SendMessage(const FString& Message);
+private:
+	void OnCompleted(int32 returnCode);
 };
